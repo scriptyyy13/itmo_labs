341244472a6e4618163f931ebc62d7f8c0671f6b
Scriptyyy <81494246+scriptyyy13@users.noreply.github.com>
2025-10-05 12:13:42 +0300

    fix: вывод матрицы и вычисление элемента массива были вынесены в отдельные методы

--- proga/sem01/laba01/Main.java
+++ proga/sem01/laba01/Main.java
@@ -1,6 +1,5 @@
 import java.util.Random;
 
-
 public class Main {
     public static void main(String[] args) {
         short[] w = new short[12];
@@ -8,67 +7,66 @@ public class Main {
             w[i] = (short)(2 * i + 1);
         }
 
-
         double[] x = new double[16];
         Random rnd = new Random();
         for (int j = 0; j < 16; j++) {
             x[j] = -4.0 + 16.0 * rnd.nextDouble();
         }
 
-
         double[][] s = new double[12][16];
-
 
         for (int i = 0; i < 12; i++) {
             for (int j = 0; j < 16; j++) {
-                double val = x[j];
-
-                if (w[i] == 7) {
-                    // формула 1
-                    double sqrtAbs = Math.sqrt(Math.abs(val));
-                    if (sqrtAbs > 0) {
-                        double logVal = Math.log(sqrtAbs);
-                        double denominator = Math.PI - Math.cos(Math.atan((val + 4) / 16.0));
-                        if (denominator != 0) {
-                            s[i][j] = Math.pow(logVal, 0.5 / denominator);
-                        } else {
-                            s[i][j] = Double.NaN;
-                        }
-                    } else {
-                        s[i][j] = Double.NaN;
-                    }
-                }
-
-                else if (w[i] == 1 || w[i] == 3 || w[i] == 9 || w[i] == 15 || w[i] == 17 || w[i] == 21) {
-                    // формула 2
-                    double tanVal = Math.tan(val);
-                    if (tanVal != 0) {
-                        s[i][j] = (1.0 / 3.0) * (0.5 - Math.pow(0.5 / tanVal, 2));
-                    } else {
-                        s[i][j] = Double.NaN;
-                    }
-                }
-
-                else {
-                    // формула 3
-                    double a = Math.pow(Math.exp(val), Math.pow((val - 1) / 4.0, 3) / 2.0) / 2.0;
-                    if (val != 0) {
-                        double b = Math.pow(2.0 / Math.cos(val), Math.pow((val + 2.0 / 3.0) / val, 2));
-                        s[i][j] = Math.cbrt(Math.pow(a, b));
-                    } else {
-                        s[i][j] = Double.NaN;
-                    }
-                }
+                s[i][j] = computeElement(w[i], x[j]);
             }
         }
 
+        printMatrix(s);
+    }
 
-        for (int i = 0; i < 12; i++) {
-            for (int j = 0; j < 16; j++) {
-                System.out.printf("%12.3f", s[i][j]);
+
+    // вычисление элемента массива
+    public static double computeElement(short wVal, double xVal) {
+        if (wVal == 7) {
+            // формула 1
+            double sqrtAbs = Math.sqrt(Math.abs(xVal));
+            if (sqrtAbs > 0) {
+                double logVal = Math.log(sqrtAbs);
+                double denominator = Math.PI - Math.cos(Math.atan((xVal + 4) / 16.0));
+                if (denominator != 0) {
+                    return Math.pow(logVal, 0.5 / denominator);
+                } else {
+                    return Double.NaN;
+                }
+            } else {
+                return Double.NaN;
             }
+        } else if (wVal == 1 || wVal == 3 || wVal == 9 || wVal == 15 || wVal == 17 || wVal == 21) {
+            // формула 2
+            double tanVal = Math.tan(xVal);
+            if (tanVal != 0) {
+                return (1.0 / 3.0) * (0.5 - Math.pow(0.5 / tanVal, 2));
+            } else {
+                return Double.NaN;
+            }
+        } else {
+            // формула 3
+            double a = Math.pow(Math.exp(xVal), Math.pow((xVal - 1) / 4.0, 3) / 2.0) / 2.0;
+            if (xVal != 0) {
+                double b = Math.pow(2.0 / Math.cos(xVal), Math.pow((xVal + 2.0 / 3.0) / xVal, 2));
+                return Math.cbrt(Math.pow(a, b));
+            } else {
+                return Double.NaN;
+            }
+        }
+    }
 
-            System.out.println();
+    // печать матрицы
+    public static void printMatrix(double[][] matrix) {
+        for (double[] row : matrix) {
+            for (double value : row) {
+                System.out.printf("%12.3e", value);
+            } System.out.println();
         }
     }
 }
