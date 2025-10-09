107f76c061fd2c0e9fa5021bf1aa3fafd52a9c50
Scriptyyy <81494246+scriptyyy13@users.noreply.github.com>
2025-10-09 17:47:09 +0300

    Update prog_laba01.py

--- infa/sem01/laba01/prog_laba01.py
+++ infa/sem01/laba01/prog_laba01.py
@@ -1,16 +1,17 @@
-def conv(num, f, t):
-    n = int(num, f)
-    if t == 10:
-        return str(n)
-    s = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    r = ""
-    while n > 0:
-        r = s[n % t] + r
-        n //= t
-    return r or "0"
-
-num = input()
-f = int(input())
-t = int(input())
-
-print(conv(num, f, t))
+def conv(num, f, t): # ПСЖ
+    n = int(num, f) # ПСЖ
+    if t == 10: # ПСЖ
+        return str(n) # ПСЖ
+    s = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" # ПСЖ
+    r = "" # ПСЖ
+    while n > 0: # ПСЖ
+        r = s[n % t] + r # ПСЖ
+        n //= t # ПСЖ
+    return r or "0" # ПСЖ
+ # ПСЖ
+num = input() # ПСЖ
+f = int(input()) # ПСЖ
+t = int(input()) # ПСЖ
+ # ПСЖ
+ # ПСЖ
+print(conv(num, f, t)) # ПСЖ
